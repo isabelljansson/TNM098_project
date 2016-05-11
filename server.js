33ba9d62ddc1e7b6b74503adf80be31145902e26
Isabell Jansson <isaja187@student.liu.se>
2016-05-11 09:33:59 +0200

    connect to mongo db with mongoose, require a file auth.js with username and password, will not be pushed to github

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 var express = require('express');
+var mongoose = require('mongoose')
+var config = require('./auth.js')
 var app = express();
 
 app.use(express.static(__dirname + '/public')); 
+
+mongoose.connect('mongodb://' + config.user + ':' + config.pwd + '@127.0.0.1:27017/tnm098');
 
 app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
 app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+  console.log('App listening on port 3000!');
 });
