9e7e99657e3bc65cc7a33e08e4e60249063a972e
jonathanbosson <jonathan.bosson@gmail.com>
2016-05-12 15:18:55 +0200

    Swap floor and view np

--- public/js/main.js
+++ public/js/main.js
@@ -17,7 +17,7 @@ var mapDiv = $("#map");
 
 var margin = {top: 20, right: 20, bottom: 20, left: 20},
 width = mapDiv.width() - margin.right - margin.left,
-        height = mapDiv.height() - margin.top - margin.bottom;
+height = mapDiv.height() - margin.top - margin.bottom;
 
 //Sets the colormap
 var colors = colorbrewer.Set3[10];
@@ -26,8 +26,6 @@ var colors = colorbrewer.Set3[10];
 var svg = d3.select("#map").append("svg")
         .attr("width", width)
         .attr("height", height)
-        .append("image")
-        .attr("xlink:href", "floor1.jpg");
         .call(zoom);
 
 var g = svg.append("g");
@@ -41,30 +39,92 @@ var projection = d3.geo.mercator()
 var path = d3.geo.path().projection(projection);
 
 //Loads geo data
-d3.json("json/proxMobileOut-MC2.json", function (error, people) {
+d3.json("json/proxMobileOut-MC2.json", function (json) {
+    console.log(json);
+    //root = json;
+    //root.fixed = true;
+    //root.x = w / 2;
+    //root.y = h / 4;
+
+    var defs = svg.insert("svg:defs")
+      .data(["end"]);
+
     //var countries = topojson.feature(people, data.message).features;
-    //draw(people);
+    //console.log(people.message);
+    draw();
 });
+
+// temp dataset to plot out max/min of x,y [0,0] -> [189,111]
+    var dataset = [
+                [0, 0], [189, 111], [57, 50], [100, 33], [150, 95],
+                [10, 12], [175, 44], [25, 67], [85, 21], [30, 88]
+            ];
+
+var currImg = 0;
+var currView = 0;
+var offsetX = 160, offsetY = -70;
+var maxX = 189, maxY = 115;
+
+var imageList = [
+        "images/floor1.jpg",        // 0
+        "images/energyz_F1.jpg",
+        "images/proxz_F1.jpg",
+        "images/floor2.jpg",        // 3
+        "images/energyz_F2.jpg",
+        "images/proxz_F2.jpg",
+        "images/floor3.jpg",        // 6
+        "images/energyz_F3.jpg",
+        "images/proxz_F3.jpg"
+    ];
+
+console.log("Index: "  + (currImg*3 + currView));
+console.log(imageList[currImg*3 + currView]);
+
+this.changeFloor = function() {
+    if (currImg > 1)
+        currImg = 0;
+    else
+        currImg++;
+    draw();
+}
+
+this.toggleView = function() {
+    if (currView >= 2)
+        currView = 0;
+    else
+        currView++;
+    draw();
+}
+
 
 
 
 //Draws the map and the points
-function draw(countries)
+function draw()
 {
-    //draw map
-    var country = g.selectAll(".country").data(countries);
-    country.enter().insert("path")
-            .attr("class", "country")
-            .attr("d", path)
-            .style('stroke-width', 1)
-            .style("fill", "lightgray")
-            .style("stroke", "white");
+    d3.select("g").remove();
+    var g = svg.append("g");
+
+    //draw map of office floor
+    var people = g
+         .attr("x",0)
+         .attr("y",0)
+         .attr("transform","translate(0,0)")
+         .append('image')
+         .attr("x",0)
+         .attr("y",0)
+         .attr("width",width)
+         .attr("height",height)
+         .attr("xlink:href",imageList[currImg*3 + currView]);
 
     //draw point        
-	var point = g.selectAll(".point").data(geoData.features)
-		.enter().append("path")
+	var point = g.selectAll("circle").data(dataset)
+		.enter().append("circle")
 		.attr("d", path)
-		.attr("class", "point");
+		.attr("class", "point")
+        .attr("cx", function(d) { return (d[0]*width/(1.6*maxX) + offsetX); })
+        .attr("cy", function(d) { return (height - (d[1]*height/(1.4*maxY)) + offsetY); })
+        .attr("r", 5);
 };
 
 //Zoom and panning method
