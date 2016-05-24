9c6524c6d3e0cb179cb31212f394beed01e4cfc3
ronjagrosz <ronja_ronja@msn.com>
2016-05-24 21:08:59 +0200

    added css to main.js

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-var express = require('express');
+ar express = require('express');
 //var mongoose = require('mongoose')
 var MongoClient = require('mongodb').MongoClient;
 var bodyParser = require('body-parser')
